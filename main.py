aae1cccd5cde3d50cd6c3164d67a129156429b60
Vladimir Mukhin <vlad@mukhin.page>
2020-11-29 12:09:49 -0500

    Print only required headers

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ from random import randrange
 import json
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
+
     def do_HEAD(self):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+
     def do_GET(self):
-        """Respond to a GET request."""
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
+
+        print(f"x-request-id      : {self.headers['x-request-id']}")
+        print(f"x-b3-traceid      : {self.headers['x-b3-traceid']}")
+        print(f"x-b3-spanid       : {self.headers['x-b3-spanid']}")
+        print(f"x-b3-parentspanid : {self.headers['x-b3-parentspanid']}")
+        print(f"x-b3-sampled      : {self.headers['x-b3-sampled']}")
+        print(f"x-b3-flags        : {self.headers['x-b3-flags']}")
+        print(f"b3                : {self.headers['b3']}")
+
+        
         self.wfile.write(bytes(json.dumps({'altitude': self.generateCoordinates('alt'), 'latitude': self.generateCoordinates('lat')}), 'utf-8'))
 
     def generateCoordinates(self, coordinate_type):
@@ -21,9 +32,7 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
         if coordinate_type == 'lat':
             return randrange(-180,180)
 
-
 PORT = 8001
-
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
